14f9fa983b98297aba48678c5aed5b2dc2b2b6ef
Tufan YILMAZ <yilmaztufan27@gmail.com>
2025-02-15 17:09:50 +0300

    Quary repositor eklendi

---
+++ TwoFun.GenericRepository/Toolbox/IQueryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoFun.GenericRepository.Toolbox
+{
+    public interface IQueryRepository
+    {
+        IQueryable<TEntity> GetQueryable<TEntity>()
+           where TEntity : class;
+        Task<List<T>> GetListAsync<T>(
+           Expression<Func<T, bool>> condition,
+           Func<IQueryable<T>, IIncludableQueryable<T, object>> includes,
+           bool asNoTracking,
+           CancellationToken cancellationToken = default)
+            where T : class;
+
+    }
+}
