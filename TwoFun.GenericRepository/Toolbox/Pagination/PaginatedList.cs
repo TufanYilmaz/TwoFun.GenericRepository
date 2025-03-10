8bd9779808da4ff45bce67b6ee3b41bb870e0068
SANKOHOLDING\tufan.yilmaz <tufan.yilmaz@sanko.com.tr>
2025-03-10 09:13:47 +0300

    paginagtions tool eklendi

---
+++ TwoFun.GenericRepository/Toolbox/Pagination/PaginatedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoFun.GenericRepository.Toolbox.Pagination
+{
+    public class PaginatedList<T> 
+        where T: class
+    {
+        public PaginatedList(List<T> items, long totalItems, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalPages = (int)Math.Ceiling(totalItems / (double)pageSize);
+            TotalItems = totalItems;
+            Items = new List<T>(pageSize);
+            Items.AddRange(items);
+        }
+
+        // This is for serialization.
+        private PaginatedList()
+        {
+        }
+
+        public int PageIndex { get; }
+
+        public int PageSize { get; }
+
+        public int TotalPages { get; }
+
+        public long TotalItems { get; }
+
+        public List<T> Items { get; }
+    }
+}
---
+++ TwoFun.GenericRepository/Toolbox/Specifications/PaginationSpecification.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwoFun.GenericRepository.Toolbox.Specifications
+{
+    public class PaginationSpecification<T> : SpecificationBase<T>
+        where T: class
+    {
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+    }
+}
